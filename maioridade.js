64bb4dc8131e6c3e4ea3d142531ea5f512dc80fa
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2024-10-08 17:34:33 -0300

    resolucao maioridade

---
+++ maioridade.js
@@ -0,0 +1,13 @@
+
+function verificarIdade(idade) {
+    if (idade >= 18) {
+      console.log("Você é maior de idade.");
+    } else {
+      console.log("Você é menor de idade.");
+    }
+  }
+  
+  
+  const idadePessoa = (7) 
+  verificarIdade(parseInt(idadePessoa)); 
+  
