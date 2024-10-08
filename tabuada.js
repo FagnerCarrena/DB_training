b525f9922bad6531f9b5441e0f8164916459038a
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2024-10-08 17:15:07 -0300

    resolvendo o exercicio da tabuada

---
+++ tabuada.js
@@ -0,0 +1,12 @@
+
+function Tabuada(numero) {
+    console.log(`Tabuada do ${numero}:`);
+    
+    for (let i = 1; i <= 10; i++) {
+      console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+  }
+  
+  const digiteNumero = 170; 
+  Tabuada(digiteNumero);
+  
